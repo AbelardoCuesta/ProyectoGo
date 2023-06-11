7f7fc9f46ff0138fec348572a004b8ff8820b190
Abelardo Cuesta <j.cuestaa@uniandes.edu.co>
2023-06-11 07:48:51 -0500

    commit primeros packages

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"example.com/m/numeros"
 	"example.com/m/variables"
 )
 
 func main() {
 	variables.Muestro()
+	numeros.MuestroNumeros()
 
 }
---
+++ numeros/ejemplo.go
@@ -0,0 +1,14 @@
+package numeros
+
+import (
+	"fmt"
+)
+
+func MuestroNumeros() {
+
+	var intcomunt int = 11
+	int32 := int32(22)
+	fmt.Println(intcomunt)
+	fmt.Println(int32)
+
+}
