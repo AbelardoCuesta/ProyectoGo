753726670bebcfcf9248338fc6f8c6ea3b3ddc19
Abelardo Cuesta <j.cuestaa@uniandes.edu.co>
2023-06-11 08:17:15 -0500

    avance 3

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"example.com/m/numeros"
 	"example.com/m/variables"
 )
 
 func main() {
-	variables.Muestro()
-	numeros.MuestroNumeros()
+	variables.RestoVariables()
 
 }
