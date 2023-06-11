891f036f431bafb986d26b7ba579c069b4aa98c9
Abelardo Cuesta <j.cuestaa@uniandes.edu.co>
2023-06-11 07:42:10 -0500

    commit primeros packages

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"example.com/m/variables"
+)
+
+func main() {
+	variables.Muestro()
+
+}
---
+++ variables/enteros.go
@@ -0,0 +1,14 @@
+package variables
+
+import (
+	"fmt"
+)
+
+func Muestro() {
+
+	var intcomunt int = 9
+	int32 := int32(30)
+	fmt.Println(intcomunt)
+	fmt.Println(int32)
+
+}
